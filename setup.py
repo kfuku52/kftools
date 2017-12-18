46f8e162a7246a2ca03295170671621fce52261e
Kenji Fukushima <kfuku52@gmail.com>
2017-12-18 11:04:33 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+        name             = 'kftools',
+        version          = "0.1",
+        description      = 'Utility tools for personal use',
+        license          = "BSD 3-clause License",
+        author           = "Kenji Fukushima",
+        author_email     = 'kfuku52@gmail.com',
+        url              = 'https://github.com/kfuku52/kftools.git',
+        keywords         = '',
+        packages         = find_packages(),
+        install_requires = [],
+        )
